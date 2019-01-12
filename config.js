d4572ba478433b50dcf537fbe26a30f889603cb0
abdousmounir <mounirabdous@gmail.com>
2019-01-12 14:59:45 -0500

    config...

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAARQjccneVkBAIKjDLHqhTV46OnsJrlxEwzRQh3j6PVo6AIzj5MZBIqpc6cZAW31N9zrqzyAu7aZAMoHz44ZCIIJnjQ0ZC7LMLyT6bUbdZBSYrnSpWgggKlRgjOvziJPJa5Jnlz6ZATZCZCV8ZAlSzDYuzx90Q2MlprZADasuZBwEjOMjf33IY2aLIblpP24jqfA9WUZD',
+    FB_VERIFY_TOKEN: 'my-chat-bot-token-mounir',
+    FB_APP_SECRET: 'b9f9ebd8fde0d0ede8c43cac2fa264e8',
+    SERVER_URL: 'https://chat-bot-mounir.herokuapp.com/',
+    GOOGLE_PROJECT_ID: 'chatbot3-182af',
+    DF_LANGUAGE_CODE: 'FR',
+    GOOGLE_CLIENT_EMAIL: 'dialogflow-knqddk@chatbot3-182af.iam.gserviceaccount.com',
+    GOOGLE_PRIVATE_KEY: 'ec410197685a4f88b08cae29a2cbdf4a07a2fa10'
+};
