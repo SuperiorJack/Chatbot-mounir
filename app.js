150dcff7a20bff79a8022ee4bd502866380c45b4
abdousmounir <mounirabdous@gmail.com>
2019-01-12 23:10:18 -0500

    await

--- app.js
+++ app.js
@@ -877,7 +877,7 @@ function isDefined(obj) {
 }
 
 function startMessage(senderID) {
-    var user = getUserInfo(senderID);
+    var user = await getUserInfo(senderID);
     if (user.first_name) {
         sendTextMessage(senderID, "Bonjour " + user.first_name + "! Que puis-je faire pour toi?");
     } else {
